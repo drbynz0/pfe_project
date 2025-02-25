b367c2cb9a8a353f4718f5b815234f29340326ce
saadnaikram <saadnaikram0704@gmail.com>
2025-02-25 17:20:19 +0000

    ikram

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '/utils/profile_page.dart';
-import '/screens/login_page/login_page.dart';
-import 'screens/ens_pages/ens_home_page.dart';
-import 'screens/etudiant_pages/etud_home_page.dart';
-import 'screens/cond_pages/cond_home_page.dart';
-import '/services/matiere_service.dart';
-import 'screens/parent_pages/parent_home_page.dart';
+import 'firebase_options.dart'; // Importez ce fichier après l'avoir généré
 
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => MatiereService()),
-      ],
-      child: const MyApp(),
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color.fromARGB(255, 31, 34, 72),
-          selectedItemColor: Color.fromARGB(255, 45, 123, 220),
-          unselectedItemColor: Color.fromARGB(255, 87, 99, 108),
-        ),
-      ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/homeEns': (context) => const TeacherHomePage(),
-        '/homeEtud': (context) => const EtudiantHomePage(),
-        '/homeCond':(context) =>  const CondHomePage(),
-        '/homePar':(context) =>  const ParentHomePage(),
-        '/profile': (context) => const ProfilePage(),
-      },
+      title: 'Gestion Scolaire',
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Firebase Intégré 🚀")),
+        body: const Center(child: Text("Firebase est prêt !")),
+      ),
     );
   }
 }
